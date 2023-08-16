406a82d527905e11a6a2bcc85d1ad4d48b50e3ce
Geoffrey-Anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2023-08-16 17:35:23 +0530

    ADDED Buying products

--- api/src/routes/Admin/index.ts
+++ api/src/routes/Admin/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./Auth";
+import OrderRouter from "./Order";
 
 const AdminRouter = Router();
 
 AdminRouter.use("/auth", authRouter);
+AdminRouter.use("/order", OrderRouter);
 
 export default AdminRouter;
