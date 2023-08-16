de69a2d1ebd414d070a340e1143fa1fe0a2c11ac
Geoffrey-Anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2023-08-16 16:01:30 +0530

    ADDED AUTH

--- api/src/routes/Customer/Auth.ts
+++ api/src/routes/Customer/Auth.ts
@@ -1,13 +1,74 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
 
-const authRouter = Router();
+type Customer = {
+  id: number;
+  email: string;
+  password: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
-authRouter.post("/register", (req, res) => {
-  res.send("Register");
+const prisma = new PrismaClient();
+
+const customerRouter = Router();
+
+customerRouter.post("/register", async (req, res) => {
+  const { email, password, name } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const newCustomer = await prisma.customer.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        role: "CUSTOMER",
+      },
+    });
+
+    res
+      .status(201)
+      .json({ message: "Registration successful", customer: newCustomer });
+  } catch (error) {
+    console.error("Error registering customer:", error);
+    res.status(500).json({ error: "Registration failed" });
+  }
 });
 
-authRouter.post("/login", (req, res) => {
-  res.send("Login");
+customerRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const customer = await prisma.customer.findUnique({ where: { email } });
+
+    if (!customer) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, customer.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: customer.id, role: customer.role, name: customer.name },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.json({ message: "Login successful", token });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ error: "Login failed" });
+  }
 });
 
-export default authRouter;
+export default customerRouter;
--- api/src/routes/Retailer/Auth.ts
+++ api/src/routes/Retailer/Auth.ts
@@ -1,13 +1,75 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
 
-const authRouter = Router();
+type Retailer = {
+  id: number;
+  email: string;
+  password: string;
+  name: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
-authRouter.post("/register", (req, res) => {
-  res.send("Register");
+const prisma = new PrismaClient();
+
+const retailerRouter = Router();
+
+retailerRouter.post("/register", async (req, res) => {
+  const { email, password, name, role } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const newRetailer = await prisma.retailer.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        role,
+      },
+    });
+
+    res
+      .status(201)
+      .json({ message: "Registration successful", retailer: newRetailer });
+  } catch (error) {
+    console.error("Error registering retailer:", error);
+    res.status(500).json({ error: "Registration failed" });
+  }
 });
 
-authRouter.post("/login", (req, res) => {
-  res.send("Login");
+retailerRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const retailer = await prisma.retailer.findUnique({ where: { email } });
+
+    if (!retailer) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, retailer.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: retailer.id, role: retailer.role, name: retailer.name },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.json({ message: "Login successful", token });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ error: "Login failed" });
+  }
 });
 
-export default authRouter;
+export default retailerRouter;
